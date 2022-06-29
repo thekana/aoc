75480fb5e0a804c0913720ec2e31a4ca2fbf3e0d
Kanadech J <kanadech.king@gmail.com>
2022-06-29 23:07:55 +0700

    clean up

--- scala/src/main/scala/y2021/Day1.scala
+++ scala/src/main/scala/y2021/Day1.scala
@@ -9,14 +9,11 @@ object Day1 {
   def part1(input: Seq[Int]): Int = {
     input.foldLeft[(Int, Option[Int])]((0, None)) {
       (acc, current) => {
-        acc._2 match {
-          case Some(value) => if (value < current) {
-            (acc._1 + 1, Some(current))
-          } else {
-            (acc._1, Some(current))
-          }
-          case _ => (acc._1, Some(current))
+        val result = acc._2 match {
+          case Some(value) if value < current => 1
+          case _ => 0
         }
+        (acc._1 + result, Some(current))
       }
     }._1
   }
